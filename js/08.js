c1714535cfdc10c068b595d06c1d779528983cb2
AegisVP <vlad@pysarenko.com>
2022-06-20 01:13:01 +0200

    done 8

--- js/08.js
+++ js/08.js
@@ -1,5 +1,31 @@
 // #region Завдання 8
 console.log("Section 8 start");
+const formEl = document.querySelector("form.login-form");
+
+formEl.addEventListener("submit", validateFormSubmit);
+
+function validateFormSubmit(form) {
+	const returnUser = {};
+
+	form.preventDefault();
+	console.log("validating");
+
+	for (const child of formEl.querySelectorAll("input")) {
+		console.log(`${child?.name} = "${child?.value}"`);
+
+    if (!child?.value) {
+      window.alert("All fields must be filled out!");
+      return;
+    } else {
+      returnUser[child.name] = child?.value;
+    }
+	}
+
+	console.log(returnUser);
+
+	this.reset();
+}
+
 console.log("Section 8 end");
 console.log("--------------------------");
 // #endregion
